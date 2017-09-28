ace289f2a4c551643f3aaae3f251261e0f1ef044
esther98 <31944414+esther98@users.noreply.github.com>
2017-09-27 17:22:56 -0700

    Create animals.js

---
+++ animals.js
@@ -0,0 +1,32 @@
+<html>
+    <head><script>
+    function pig(){
+        alert("sheep");
+        alert(x);
+        </script>}
+            </script>
+            <script type="text/javascript">var count = 6 </script>
+            </head>
+
+    <body>
+        
+
+        <H1><li>pig</li></H1>
+        
+        <button onclick="pig();">enter an animal.</button><input>
+        <H2><li>Cow</li></H2>
+        <button onclick="cow();">enter an animal.</button><input>
+        <H3><li>elephant</li></H3>
+        <button onclick="elephant();">enter an animal.</button><input>
+        <H3><li>Dog</li></H3>
+        <button onclick="dog();">enter an animal.</button><input>
+        <H4><li>Lion</li></H4>
+        <button onclick="lion();">enter an animal.</button><input>
+        <H5><li>Rhino</li></H5>
+        <button onclick="Rhino();">enter an animal.</button><input>
+        <H6><Li>Hippo</Li></H6>
+        <button onclick="hippo();">enter an animal</button><input>
+
+
+    </body>
+    </html>
